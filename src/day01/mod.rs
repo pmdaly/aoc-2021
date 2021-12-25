f8dfe129530e9fbce913192941b2e39117402244
Patrick Daly <pmdaly12@gmail.com>
2021-12-24 18:04:16 -0800

    day1 part b done

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -3,7 +3,8 @@ use std::io::prelude::*;
 use std::fs::File;
 
 pub fn part1() -> u32 {
-    let file = File::open("./src/day01/input.txt").expect("file was not found");
+    let file = File::open("./src/day01/input.txt")
+        .expect("file was not found");
     let reader = BufReader::new(file);
     let vals: Vec<u32> = reader
         .lines()
@@ -12,6 +13,25 @@ pub fn part1() -> u32 {
     let mut increased = 0;
     for (i, val) in vals.iter().enumerate() {
         if (i > 0) && (val > &vals[i-1]) {
+            increased += 1;
+        }
+    }
+    return increased;
+}
+
+pub fn part2() -> u32 {
+    let file = File::open("./src/day01/input.txt")
+        .expect("file was not found");
+    let reader = BufReader::new(file);
+    let vals: Vec<u32> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<u32>().unwrap())
+        .collect();
+    let mut increased = 0;
+    for i in 3..vals.len() {
+        let current_window: u32 = vals[i-2..i+1].iter().sum();
+        let previous_window: u32 = vals[i-3..i].iter().sum();
+        if current_window > previous_window {
             increased += 1;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod day01;
 
 fn main() {
-    let result = day01::part1();
-    println!("{}", result);
+    println!("Day 1a: {}", day01::part1());
+    println!("Day 1b: {}", day01::part2());
 }
